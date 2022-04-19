f79f2c29a284971b2b3a1172f64aa84bdfe4cba7
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-04-19 16:55:36 -0300

    [ADD] Default paths in webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,20 @@ module.exports = {
   //resolve va a indicar que extensiones va a estar escuchando nuestra configuracion, sobre las que va a trabajar. Particularmente solo vamos a usar .js y .jsx para react.
   resolve: {
     extensions: ['.js', '.jsx'],
+    alias: {
+      '@adapters': path.resolve(__dirname, 'src/adapters'), // Los adapters nos adaptan la información, formatean a una forma que nosotros queramos y la devuelven.
+      '@assets': path.resolve(__dirname, 'src/assets'), // multimedia, tipografía, etc.
+      '@components': path.resolve(__dirname, 'src/components'), // componentes que se usen en TODA la aplicacion
+      '@contexts': path.resolve(__dirname, 'src/contexts'), // Manejo del estado diferente a Redux, cosas que solamente se encuentren dentro de una vista o simples y no valdria la pena meterlas en redux, mientras que en redux metemos cosas de toda la aplicación como información del usuario. RECOMENDADO USAR DENTRO DE UNA PAGE
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@interceptors': path.resolve(__dirname, 'src/interceptors'), //Hace una petición y si en la cabecera no se encuentra por ejemplo el token, lo intercepta. Por ejemplo que un usuario no esté autentificado.
+      '@models': path.resolve(__dirname, 'src/models'), //TYPESRIPT, representaciones de nuestras entidades.
+      '@pages': path.resolve(__dirname, 'src/pages'), //Las distintas Vistas que vamos a tener, dentro de ellas pueden tener las mismas carpetas de src en esta lista pero con las cosas particulares de cada vista.
+      '@redux': path.resolve(__dirname, 'src/redux'), //Manejo de los estados, el store
+      '@services': path.resolve(__dirname, 'src/services'), // Los servicios son las llamadas a las apis,
+      '@styles': path.resolve(__dirname, 'src/styles'), // Estilos
+      '@utilities': path.resolve(__dirname, 'src/utilities'), // Muchas veces vamos a reutilizar lógica y esa lógica hay que almacenarla en algún lado. Es lógica que sacada de contexto funciona en distintos lugares.
+    },
   },
   //El modulo va a tener ciertas reglas necesarias para la construccion de nuestro producto
   module: {
